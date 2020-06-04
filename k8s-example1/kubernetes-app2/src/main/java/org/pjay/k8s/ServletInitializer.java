3f020993203d6d0d3242fcb7a3a332af7fafa687
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-06-04 23:13:35 +0530

    added app2 skeleton

---
+++ k8s-example1/kubernetes-app2/src/main/java/org/pjay/k8s/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.pjay.k8s;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KubernetesApp2Application.class);
+	}
+
+}
