b1a00d3531796b4b511705a90f12488b9ab04b55
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-06-04 23:06:26 +0530

    added app1 skeleton

---
+++ k8s-example1/kubernetes-app1/src/main/java/org/pjay/k8s/KubernetesApp1Application.java
@@ -0,0 +1,13 @@
+package org.pjay.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesApp1Application.class, args);
+	}
+
+}
