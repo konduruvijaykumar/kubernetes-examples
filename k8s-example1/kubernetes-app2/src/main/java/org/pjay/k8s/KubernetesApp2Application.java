f3ea973ffacf17723b0ee26d922d5de2f7303cac
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-06-05 22:39:00 +0530

    join servletinitializer and springboot app class

--- k8s-example1/kubernetes-app2/src/main/java/org/pjay/k8s/KubernetesApp2Application.java
+++ k8s-example1/kubernetes-app2/src/main/java/org/pjay/k8s/KubernetesApp2Application.java
@@ -2,12 +2,23 @@ package org.pjay.k8s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author vijayk
+ *
+ */
 @SpringBootApplication
-public class KubernetesApp2Application {
+public class KubernetesApp2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KubernetesApp2Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KubernetesApp2Application.class);
 	}
 
 }
